b266e3b81701d03c92742ceee3dcb6e66b47ce5c
Chisom <chisomije92@gmail.com>
2022-02-01 11:00:59 +0100

    add contact api

---
+++ pages/api/contact.js
@@ -0,0 +1,30 @@
+const handler = (req, res) => {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid parameters" });
+      return;
+    }
+
+    //store in database
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+    res
+      .status(201)
+      .json({ message: "Message stored successfully", content: newMessage });
+  }
+};
